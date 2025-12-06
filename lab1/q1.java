cf7c763d24c05493dab12a8fc2cbbc8f3777f16e
voidutk <utkarsh0549@gmail.com>
2025-12-06 17:12:26 +0530

    Added OOPJ Lab 1 programs

---
+++ lab1/q1.java
@@ -0,0 +1,8 @@
+//Name class roll number
+public class q1 {
+    public static void main(String[] args) {
+        System.out.println("Name: Utkarsh");
+        System.out.println("Section: CSE 7");
+        System.out.println("Roll Number: 24155667");
+    }
+}
