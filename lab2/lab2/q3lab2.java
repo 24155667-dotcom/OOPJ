90a28406a51fae1f8897668cf997acc0437b3d6a
voidutk <utkarsh0549@gmail.com>
2025-12-14 17:53:14 +0530

    Added q3 lab2 box volume program

---
+++ lab2/lab2/q3lab2.java
@@ -0,0 +1,35 @@
+package lab2;
+import java.util.Scanner;
+
+public class q3lab2 {
+    static class Box {
+        double length;
+        double width;
+        double height;
+
+        // Method to calculate volume
+        double volume() {
+            return length * width * height;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Box b = new Box();
+
+        System.out.print("Enter length: ");
+        b.length = sc.nextDouble();
+
+        System.out.print("Enter width: ");
+        b.width = sc.nextDouble();
+
+        System.out.print("Enter height: ");
+        b.height = sc.nextDouble();
+
+        System.out.println("Volume = " + b.volume());
+
+        sc.close();
+    }
+}
+
